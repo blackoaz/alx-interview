711ef35b2b9b48062206e3bb27f83b1e77df4e58
blackoaz <pauloakello56@gmail.com>
2023-10-04 12:05:07 +0000

    adding task for pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Function for building a pascal triangle"""
+
+
+def pascal_triangle(n):
+    """
+    Building arrays for Pascals triangle
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    while len(triangle) != n:
+        previous = triangle[-1]
+        current = [1]
+        for i in range(len(previous) - 1):
+            current.append(previous[i] + previous[i + 1])
+        current.append(1)
+        triangle.append(current)
+    return triangle
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+Writting function for Pascal Triangle
