b65ea3c480a8540fa22f86af52045f38717b46a3
blackoaz <pauloakello56@gmail.com>
2023-10-15 23:10:52 +0300

    updating to pep 8

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+write a method that calculates the fewest number of
+operations needed to result in exactly n H characters in the file.
 """
 
 
